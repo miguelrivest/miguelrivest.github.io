5eac06d5d0ed9bbfd3013ce99cb634e9c40b847e
Miguel Rivest <miguelrivest@outlook.com>
2024-02-13 14:38:54 -0500

    Fixing the translation script

--- js/translate.js
+++ js/translate.js
@@ -34,25 +34,57 @@ let frenchButton = document.getElementById("french");
 let englishButton = document.getElementById("english");
 
 frenchButton.addEventListener('click', function () {
-    let elements = document.querySelectorAll("h1,h2,h3,p,button,a");
+    let elements = document.querySelectorAll("h1,h2,h3,p,span,td,th,button,a,footer");
 
     for (let element of elements) {
         if (!element.hasAttribute('noTranslate')) {
-            let message = element.innerText;
-            translate(message).then(translatedMessage => {
-                element.innerText = translatedMessage;
+            currentTranslation = element.innerText;
+            translate(currentTranslation).then(translatedMessage => {
+                element.innerText = parseHtmlEntities(translatedMessage);
             });
         }
     }
-})
+});
 
 englishButton.addEventListener('click', function () {
-    let elements = document.querySelectorAll("h1,h2,h3,p");
+    let elements = document.querySelectorAll("h1,h2,h3,p,span,td,th,button,a,footer");
 
     for (let element of elements) {
-        let message = element.innerText;
-        translateEn(message).then(translatedMessage => {
-            element.innerText = translatedMessage;
-        });
+        if (!element.hasAttribute('noTranslate')) {
+            currentTranslation = element.innerText;
+            translateEn(currentTranslation).then(translatedMessage => {
+                element.innerText = parseHtmlEntities(translatedMessage);
+            });
+        }
     }
-})
+});
+
+function getText(element) {
+    let childNodes = element.childNodes;
+    let text = '';
+
+    for (let i = 0; i < childNodes.length; i++) {
+        if (childNodes[i].nodeType === Node.TEXT_NODE) {
+            text += childNodes[i].textContent;
+        }
+    }
+
+    return text;
+}
+
+function setText(element, text) {
+    let childNodes = element.childNodes;
+
+    for (let i = 0; i < childNodes.length; i++) {
+        if (childNodes[i].nodeType === Node.TEXT_NODE) {
+            childNodes[i].textContent = text;
+            break;
+        }
+    }
+}
+
+function parseHtmlEntities(text) {
+    let parser = new DOMParser();
+    let doc = parser.parseFromString(text, 'text/html');
+    return doc.body.textContent;
+}
